5ee8711e1f197863fdcd07618bdff790e0a242ad
Hoang Nam <ntranhoang1@192.168.2.11>
2022-06-10 10:16:35 +0700

    Add convexHull problem

--- LAB/LAB01/4.cpp
+++ LAB/LAB01/4.cpp
@@ -4,11 +4,42 @@
 #include <fstream>
 #include <vector>
 #include <limits.h>
+#include <math.h>
+#include <unordered_set>
 using namespace std;
 
+const double PI = 3.14159265;
+
 struct point{
-    float x,y;
+    double x,y;
 };
+
+double computeAngle(const point &from,const point &to)
+{
+    double angle = atan2(to.y-from.y, to.x-from.x);
+    if(angle < 0)
+        angle+=2*PI;
+    return angle;
+}
+
+int findNextPointIndex(vector<point>&points,int&curPointIndex,unordered_set<int>&inList)
+{
+    double minAngle = 2*PI;
+    int nextIndex;
+    for(int i=0;i<points.size();i++)
+    {
+        if(i==curPointIndex||inList.find(i)!=inList.end())
+            continue;
+        double angle = computeAngle(points[curPointIndex],points[i]);
+        if(angle < minAngle)
+        {
+            minAngle = angle;
+            nextIndex = i;
+        }
+    }
+    inList.insert(nextIndex);
+    return nextIndex;
+}
 
 int main()
 {
@@ -16,6 +47,7 @@ int main()
     int n;
     file>>n;
     point extremePoint;
+    int extremePointIndex;
     extremePoint.x = INT_MAX;
     extremePoint.y = INT_MAX;
     vector<point> points(n);
@@ -25,17 +57,34 @@ int main()
         file>>points[i].y;
         if(extremePoint.y>points[i].y)
         {
-            extremePoint= points[i];
+            extremePoint = points[i];
+            extremePointIndex = i;
         }
         else if(extremePoint.y==points[i].y)
         {
             if(extremePoint.x>points[i].x)
             {
-                extremePoint= points[i];
+                extremePoint = points[i];
+                extremePointIndex = i;
             }
         }
     }
-    cout<<endl;
-    cout<<extremePoint.x<<" "<<extremePoint.y<<endl;
+
+    vector<int> convexHullPoints;
+    unordered_set<int> inList;
+    convexHullPoints.push_back(extremePointIndex);
+    while(true)
+    {
+        int curPointIndex = convexHullPoints.back();
+        int nextIndex = findNextPointIndex(points,curPointIndex,inList);
+        if(nextIndex==extremePointIndex)
+            break;
+        convexHullPoints.push_back(nextIndex);
+    }
+
+    for(int i=0;i<convexHullPoints.size();i++)
+    {
+        cout<<"("<<points[convexHullPoints[i]].x<<" , "<<points[convexHullPoints[i]].y<<") ";
+    }
     return 0;
 }
